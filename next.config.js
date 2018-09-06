d652ebad8b3fd9166533ee85cdbeb302805a8feb
Sabrina Dias <dias.sabrina@gmail.com>
2018-09-06 12:01:53 -0300

    add config webpack

---
+++ next.config.js
@@ -0,0 +1,109 @@
+const webpack = require('webpack');
+const path = require('path');
+const withPlugins = require('next-compose-plugins');
+const withCss = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+const commonsChunkConfig = require('@zeit/next-css/commons-chunk-config');
+
+const configDefault = {
+	distDir: 'build',
+	poweredByHeader: false
+};
+
+const nextConfig = {
+	...configDefault,
+	webpack: (config, { dev, isServer }) => {
+
+		config.module.rules.push(
+			{
+				test: /\.(jpe?g|gif|ico)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+							outputPath: './static'
+						}
+					}
+				]
+			},
+
+			{
+				test: /\.(png)$/,
+				use: [
+					{
+						loader: 'file-loader?limit=10000&mimetype=text/html',
+						options: {
+							name:  '[path][name].[ext]',
+							outputPath: './static'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader:
+							'url-loader?limit=10000&mimetype=application/font-woff',
+						options: {
+							name: '[path][name].[ext]',
+							outputPath: './fonts'
+						}
+					}
+				]
+			},
+			{
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader:
+							'url-loader?limit=10000&mimetype=application/octet-stream'
+					}
+				]
+			},
+			{
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				use: [{ loader: 'file-loader' }]
+			},
+			{
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{ loader: 'url-loader?limit=10000&mimetype=image/svg+xml' }
+				]
+			}
+		);
+
+		if (isServer) {
+			return config;
+		}
+
+		return commonsChunkConfig(config, /\.(sass|scss)$/);
+	}
+};
+
+module.exports = withPlugins(
+	[
+		[
+			withSass,
+			{
+				cssModules: true,
+				cssLoaderOptions: {
+					importLoaders: 1,
+					localIdentName: '[path][name]__[local]'
+				}
+			}
+		],
+		[
+			withCss,
+			{
+				cssModules: true,
+				cssLoaderOptions: {
+					importLoaders: 1,
+					localIdentName: '[path][name]__[local]'
+				}
+			}
+		]
+	],
+	nextConfig
+);
