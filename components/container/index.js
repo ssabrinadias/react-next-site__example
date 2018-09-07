94a287d8cc8287cd50685fc8cca88a6e4d3c19ba
Sabrina Dias <dias.sabrina@gmail.com>
2018-09-07 16:34:24 -0300

    change css structure

--- components/container/index.js
+++ components/container/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import style from './style.scss';
-import logo from '../../static/images/RV-logo.svg'
 
 const teste = props => {
 	return (
-    	<header>
-			<img src={logo} alt='logo Red Ventures'/>
-		</header>
+		<div className={style.cores}>
+			{props.children}
+		</div>
 	);
 };
 
--- components/layout/index.js
+++ components/layout/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../header'
+import Container from '../container'
 import Menu from '../menu'
 import style from './style.scss';
 
 const teste = props => {
 	return (
 		<div>
+			<Container>tessdsdfs</Container>
 			<div className={style.top}>
 				<Header/>
 				<Menu/>
