2817cd568019ebee3281596c0e23025e914a29eb
Sabrina Dias <dias.sabrina@gmail.com>
2018-09-12 18:44:42 -0300

    add compoment carousel

--- components/wheels/index.js
+++ components/wheels/index.js
@@ -1,87 +1,108 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import {bindActionCreators} from 'redux'
-import {actionWheels} from './action';
-import currencyformat from '../currencyformat'
-import style from './style.scss';
-
+import React from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionWheels } from "./action";
+import currencyformat from "../currencyformat";
+import Slider from "react-slick";
+import style from "./style.scss";
 
 class Wheels extends React.Component {
-	constructor(props) {
-        super(props);
-        
-        this.state = {
-            selected : props.choice.id
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: props.choice.id
+    };
+  }
+
+  changeOption(e) {
+    let el = this.props.wheels.items.filter(item => item.id == e.target.id);
+
+    this.setState({
+      selected: e.target.id
+    });
+
+    return this.props.actionWheels(el[0]);
+  }
+
+  wheelsOptions({ id, label, image, price }) {
+    let activeInput = this.state.selected == id ? "checked" : "";
+    let activeClass = this.state.selected == id ? style.active : "";
+
+    return (
+      <div
+        key={id}
+        className={[activeClass, style.wheelsOptionsItem].join(" ")}
+      >
+        <label htmlFor={id}>
+          <img src={image} alt={label} />
+
+          <div className={style.description}>
+            <p>{label}</p>
+
+            <span>{price ? currencyformat(price) : "included"}</span>
+          </div>
+
+          <input
+            type="radio"
+            name="wheels"
+            id={id}
+            onChange={e => this.changeOption(e)}
+            checked={activeInput}
+          />
+        </label>
+      </div>
+    );
+  }
+
+  render() {
+    var settings = {
+      slidesToShow: 3,
+      infinite: false,
+      responsive: [
+        {
+          breakpoint: 599,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: "25%",
+            slidesToShow: 1
+          }
         }
-    }
-
-    changeOption(e) {
-        
-        let el = this.props.wheels.items
-            .filter((item)=>item.id == e.target.id)
-
-        this.setState({
-            selected : e.target.id
-        })
-
-        return (
-            this.props.actionWheels(el[0])
-        )
-    }
-
-    wheelsOptions({id, label,image, price}){
-        let activeInput = this.state.selected == id ? 'checked' : '';
-        let activeClass = this.state.selected == id ? style.active: ''; 
-        
-        return(
-            <li key={id} className={activeClass}>
-                <label htmlFor={id}>
-                    <img src={image} alt={label}/>
-                    
-                    <div className={style.description}>
-                        <p>{label}</p>
-                        
-                        <span>{price ? currencyformat(price) : 'included'}</span>
-                    </div>
-                    
-                    <input
-                        type = "radio"
-                        name = "wheels"
-                        id = {id}
-                        onChange = {e=> this.changeOption(e)}
-                        checked = {activeInput}
-                    />
-                </label>                
-            </li>
-        ) 
-    }
-    
-	render() {
-		return (
-			<section className={style.wheels}>
-                
-                <h1 className={style.title}>Wheels</h1>
-
-                <ul className={style.wheelsOptions}>
-                    {this.props.wheels.items.map(({id, label,image, price})=>(
-                        this.wheelsOptions({id, label,image, price})
-                    ))}
-                </ul>
-
-            </section>
-		);
-	}
+      ]
+    };
+    return (
+      <section className={style.wheels}>
+        <h1 className={style.title}>Wheels</h1>
+        <div className={style.wheelsOptions}>
+          <Slider {...settings}>
+            {this.props.wheels.items.map(({ id, label, image, price }) =>
+              this.wheelsOptions({ id, label, image, price })
+            )}
+          </Slider>
+        </div>
+        {/* <div>
+                    <ul className={style.wheelsOptions}>
+                        {this.props.wheels.items.map(({id, label,image, price})=>(
+                            this.wheelsOptions({id, label,image, price})
+                        ))}
+                    </ul>
+                </div> */}
+      </section>
+    );
+  }
 }
 
-const mapStateToProps = ({general:{data}, choices:{steps}}) => (
-    {           
-        wheels: data.wheels,
-        choice: steps.wheels
-    }
-);
+const mapStateToProps = ({ general: { data }, choices: { steps } }) => ({
+  wheels: data.wheels,
+  choice: steps.wheels
+});
 
-const mapDispatchToProps = (dispach) => {
-    return bindActionCreators({actionWheels},dispach)
-}
+const mapDispatchToProps = dispach => {
+  return bindActionCreators({ actionWheels }, dispach);
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Wheels);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Wheels);
--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,32 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <html>
         <Head>
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1.0, viewport-fit=contain"
-            />
-            
-            <link rel="stylesheet" href="/_next/static/style.css" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, viewport-fit=contain"
+          />
+          <link
+            rel="stylesheet"
+            type="text/css"
+            charset="UTF-8"
+            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+          />
+          <link rel="stylesheet" href="/_next/static/style.css" />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </html>
-    )
+    );
   }
 }
